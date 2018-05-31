1c3238b5d1cb9843c70b7262116aa475f4590023
Hashem Khalifa <hashem.khalifa@gmail.com>
2018-05-31 05:29:36 +0400

    rename displayed result and enhance app comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './assets/css/reset.scss';
-import DisplayedResult from './components/displayed_result';
+import DisplayedResult from './components/displayed-result';
 
-const App = () => {
-  return (
-    <div>
-      <DisplayedResult />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <DisplayedResult />
+  </div>
+);
 
 export default hot(module)(App);
