fa3951f94d908941466446a6198104e3ccce371b
Hashem Khalifa <hashem.khalifa@gmail.com>
2018-05-31 05:22:56 +0400

    convert app component to statless

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 import './assets/css/reset.scss';
 import DisplayedResult from './components/displayed_result';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <DisplayedResult />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <DisplayedResult />
+    </div>
+  );
+};
 
 export default hot(module)(App);
